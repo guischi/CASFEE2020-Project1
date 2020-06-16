059a0ee376bb13576fddfd1db08f5ec4cd873061
Guido Schilliger <guido.schilliger@gmail.com>
2020-06-16 20:00:27 +0200

    Cleanup

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,4 @@ import express from 'express';
 const router = express.Router();
 //import {indexController} from '../controller/indexController';
 
-//router.post("/login", indexController.login.bind(indexController));
-
 export const rtIndex = router;
